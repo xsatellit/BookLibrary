b72622144b0e3233d4f793351c8ab31ab8b6c027
Glenda <glendasantana2099@gmail.com>
2023-04-01 17:25:50 -0300

    Fixed Callback_SendEmail. It wasn't checking if the user had already registered an email before sendin, which caused an error if the user hadn't

--- plugins/Callback_SendEmail.py
+++ plugins/Callback_SendEmail.py
@@ -17,22 +17,27 @@ async def sendEmail_handler(app: Client, callback: CallbackQuery):
     callb, book_id = callback.data.split("_", 2)
 
     # Get the email address of the user who pressed the button
-    try:
-        user_email = database.getUserEmail(callback.from_user.id)
-    except:
+    user_email = database.getUserEmail(callback.from_user.id)
+    # Close the database connection
+    database.conn.close()
+
+    # Check if the user has an email
+    if user_email is not None:
+
+
+        # Construct the command to run the 'SendEmail.py' script
+        command = f"functions/default/SendEmail.py {user_email} {book_id}"
+        command = command.replace("\n", "")
+
+        # Execute the command to run the 'SendEmail.py' script as a subprocess
+        subprocess.Popen(r'python %s' % (command), shell=True)
+    # If the user doesn't have an email, then returns a message indicating the error
+    else:
         await app.send_message(
             callback.from_user.id,
             "Não foi possível encontrar seu email Kindle no sistema, por favor, verifique se está cadastrado utilizando o comando /email e clicando no botão Cadastrar email."
         )
 
-    # Close the database connection
-    database.conn.close()
 
-    # Construct the command to run the 'SendEmail.py' script
-    command = f"functions/default/SendEmail.py {user_email} {book_id}"
-    command = command.replace("\n", "")
-
-    # Execute the command to run the 'SendEmail.py' script as a subprocess
-    subprocess.Popen(r'python %s' % (command), shell=True)
 
 
